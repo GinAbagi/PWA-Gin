6b802ed4e68da0898aa8eb5e220c87fc25168080
ginabagi <415308@student.fontys.nl>
2021-01-11 00:36:56 +0100

    offline fallback page update

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -9,10 +10,11 @@ const assets = [
     '/css/materialize.min.css',
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v70/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v70/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+    '/pages/fallback.html'
 ];
 
-//installeren van service worker
+//install service worker //
 self.addEventListener('install', evt => {
    // console.log('service worker has been installed');
     evt.waitUntil(
@@ -27,6 +29,15 @@ self.addEventListener('install', evt => {
 //activate event
 self.addEventListener('activate', evt =>{
   //  console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys); 
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
 //fetch event
@@ -34,7 +45,12 @@ self.addEventListener('fetch', evt =>{
    // console.log('fetch event', evt);
    evt.respondWith(
        caches.match(evt.request).then(cacheRes =>{
-        return cacheRes || fetch(evt.request);
-       })
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+                cache.put(evt.request.url, fetchRes.clone());
+                return fetchRes;
+            })
+        });
+     }).catch(() => caches.match('/pages/fallback.html'))
    );
 });
