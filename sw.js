65af0e0d0b5a1911e0c6c94ecf2348257c61dab7
ginabagi <415308@student.fontys.nl>
2021-01-12 00:55:15 +0100

    render recipes from firebase

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static-v2';
+const staticCacheName = 'site-static-v1';
 const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
@@ -53,20 +53,20 @@ self.addEventListener('activate', evt =>{
 
 //fetch event
 self.addEventListener('fetch', evt =>{
-   // console.log('fetch event', evt);
-   evt.respondWith(
-       caches.match(evt.request).then(cacheRes =>{
-        return cacheRes || fetch(evt.request).then(fetchRes => {
-            return caches.open(dynamicCacheName).then(cache => {
-                cache.put(evt.request.url, fetchRes.clone());
-                limitCacheSize(dynamicCacheName, 15);
-                return fetchRes;
-            })
-        });
-     }).catch(() => {
-         if(evt.request.url.indexOf('.html') > -1){
-         return caches.match('/pages/fallback.html');
-         }
-     })
-   );
+//    // console.log('fetch event', evt);
+//    evt.respondWith(
+//        caches.match(evt.request).then(cacheRes =>{
+//         return cacheRes || fetch(evt.request).then(fetchRes => {
+//             return caches.open(dynamicCacheName).then(cache => {
+//                 cache.put(evt.request.url, fetchRes.clone());
+//                 limitCacheSize(dynamicCacheName, 15);
+//                 return fetchRes;
+//             })
+//         });
+//      }).catch(() => {
+//          if(evt.request.url.indexOf('.html') > -1){
+//          return caches.match('/pages/fallback.html');
+//          }
+//      })
+//    );
 });
